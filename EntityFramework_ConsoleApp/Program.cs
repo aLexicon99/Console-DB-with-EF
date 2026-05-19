8224812ce966197f86f3b9c7a619cdfa10fed831
aLexicon99 <267594691+aLexicon99@users.noreply.github.com>
2026-05-19 14:12:14 +0200

    rocket-class-file

--- EntityFramework_ConsoleApp/Program.cs
+++ EntityFramework_ConsoleApp/Program.cs
@@ -4,21 +4,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EntityFramework_ConsoleApp
 {
-    class Rocket
-    {
-        public int Id { get; set; }
+    //class Rocket
+    //{
+    //    public int Id { get; set; }
 
-        [MaxLength(100)]
-        public string? RocketName { get; set; }
+    //    [MaxLength(100)]
+    //    public string? RocketName { get; set; }
 
-        [MaxLength(500)]
-        public string? RocketDescription { get; set; }
-    }
+    //    [MaxLength(500)]
+    //    public string? RocketDescription { get; set; }
+    //}
     internal class Program
     {
         static void Main(string[] args)
         {
             MyDbContext Context = new MyDbContext();
+
 
             // Add new Rocket
             //
@@ -32,14 +33,19 @@ namespace EntityFramework_ConsoleApp
             //// ----------
 
 
+
             //var firstRocket = Context.Rockets.FirstOrDefault();
             //var allRockets = Context.Rockets.OrderBy(r => r.RocketName).ToList();
             //var Apollo_rocket = Context.Rockets.Find(2);
             // Eller context.Rockets.FirstOrDefault(r => r.id == 1)
 
+
+
             // One Object
             //Rocket oneRocket = Context.Rockets.FirstOrDefault(r => r.Id == 1);
             //Console.WriteLine(oneRocket.RocketName);
+
+
 
             // All Rockets
             //List<Rocket> RocketData = Context.Rockets.ToList();
@@ -47,6 +53,8 @@ namespace EntityFramework_ConsoleApp
             //{
             //    Console.WriteLine(rocket.RocketName);
             //}
+
+
 
             // INSERT/ADD Many Objects
             //List<Rocket> MyRockets = new List<Rocket>();
@@ -58,12 +66,37 @@ namespace EntityFramework_ConsoleApp
 
 
             // Find with LINQ
-            var FindRockets = Context.Rockets.Where(r => r.Id > 1).ToList();
-            foreach (Rocket rocket in FindRockets)
-            {
-                Console.WriteLine(rocket.RocketName);
-            }
+            //var FindRockets = Context.Rockets.Where(r => r.Id > 1).ToList();
+            //foreach (Rocket rocket in FindRockets)
+            //{
+            //    Console.WriteLine(rocket.RocketName);
+            //}
 
+
+            //var FindRockets = Context.Rockets.Where(r => r.Id > 1).ToList();
+            //foreach (Rocket rocket in FindRockets)
+            //{
+            //    Console.WriteLine(rocket.RocketName);
+            //}
+
+
+            // Add a new rocket
+            //Rocket NewRocket = new Rocket()
+            //{
+            //    RocketName = "Falcon 9",
+            //    RocketDescription = "Partially reusable rocket"
+            //};
+            //Context.Rockets.Add(NewRocket);
+            //Context.SaveChanges();
+
+            //Console.WriteLine(NewRocket.Id);
+            //Console.WriteLine(NewRocket.RocketName);
+
+
+            // UPDATE
+            //Rocket R1 = Context.Rockets.Single(r => r.Id == 1);
+            //R1.RocketDescription = "Space Launch System (SLS)";
+            //Context.SaveChanges();
 
             Console.WriteLine("Hello, Space!");
             //Console.WriteLine("Total Rockets : " + allRockets.Count());
@@ -71,6 +104,12 @@ namespace EntityFramework_ConsoleApp
             //Console.WriteLine(Apollo_rocket.RocketName);
             //Console.WriteLine("\n--- All Rockets -----\n");
             //allRockets.ForEach((r) => Console.WriteLine($"- {r.RocketName}, {r.RocketDescription}"));
+
+
+            foreach (Rocket rocket in Context.Rockets.ToList())
+            {
+                Console.WriteLine(rocket.RocketName);
+            }
         }
     }
 }
---
+++ EntityFramework_ConsoleApp/Rocket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFramework_ConsoleApp
+{
+    internal class Rocket
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string? RocketName { get; set; }
+
+        [MaxLength(500)]
+        public string? RocketDescription { get; set; }
+    }
+}
